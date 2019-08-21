52170ed64a254c2f9adbeee64715882a1261f942
tree <lightninghug7@gmail.com>
2019-08-21 23:42:40 +0300

    Moved Repos state to context

--- src/context/github/githubReducer.js
+++ src/context/github/githubReducer.js
@@ -19,7 +19,14 @@ export default (state, action) => {
         ...state,
         user: action.payload,
         loading: false
-      }
+      };
+    case GET_REPOS: {
+      return {
+        ...state,
+        repos: action.payload,
+        loading: false
+      };
+    }
     case CLEAR_USERS:
       return {
         ...state,
